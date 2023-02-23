3df71acac4baceed53d206e5bffacc806adda0a0
Konark-2ez <konarkkumar456@gmail.com>
2023-02-23 20:04:48 +0530

    added fw24_556 day-3 work

--- scripts/script.js
+++ scripts/script.js
@@ -76,3 +76,42 @@ fetch(url)
        localStorage.setItem("usersdata",JSON.stringify(usersData))
     })
 })
+
+let birk =  document.querySelector(".birk")
+birk.addEventListener("click",()=>{
+  location.href="./women.html"
+})
+
+
+//FOR THE FIRST IMAGE CAROUSEL
+
+
+let span1 = document.querySelector("#span1")
+let span2 = document.querySelector("#span2")
+let div = document.querySelectorAll(".sec>div")
+
+var l=0
+span2.onclick=()=>{
+  l++;
+  for(var i of  div){
+    if(l==0){i.style.left="0px"}
+    if(l==1){i.style.left="-740px"}
+    if(l==2){i.style.left="-1480px"}
+    if(l==3){i.style.left="-2220px"}
+    if(l==4){i.style.left="-2960px"}
+    if(l==5){i.style.left="-3700px"}
+    if(l>5){l=5;}
+  }
+}
+span1.onclick=()=>{
+  l--;
+  for(var i of  div){
+    if(l==0){i.style.left="0px"}
+    if(l==1){i.style.left="-740px"}
+    if(l==2){i.style.left="-1480px"}
+    if(l==3){i.style.left="-2220px"}
+    if(l==4){i.style.left="-2960px"}
+    
+    if(l<0){l=0}
+  }
+}
